3c7b38fa85a8e26781489d8f8da7fee9d176159c
mariyasaji-2023 <mariyasajichiramel.com>
2024-12-24 14:07:46 +0530

    getMenu

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,21 +1,42 @@
 const { MongoClient } = require("mongodb");
 const client = new MongoClient(process.env.DB_URI);
 const { ObjectId } = require('mongodb');
+
 const getMenu = async (req, res) => {
-    const { restaurantId } = req.body;
+    const { restaurantId, userId } = req.body;
     
     try {
         await client.connect();
         const db = client.db(process.env.DB_NAME);
         const restaurant = db.collection("restaurants");
-        
+        const User = db.collection("users");
+
+        const user = await User.findOne(
+            { _id: new ObjectId(userId) },
+            {
+                projection: {
+                    dailyCalorieGoal: 1,
+                    dailyConsumptionStats: 1
+                }
+            }
+        );
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
         const menu = await restaurant.findOne(
             { _id: new ObjectId(restaurantId) },
-            { projection: {
-                restaurantName: 1,
-                logo: 1,
-                categories: 1
-            }}
+            { 
+                projection: {
+                    restaurantName: 1,
+                    logo: 1,
+                    categories: 1
+                }
+            }
         );
         
         if (!menu) {
@@ -36,7 +57,11 @@ const getMenu = async (req, res) => {
         
         return res.status(200).json({
             success: true,
-            menu: processedMenu
+            menu: processedMenu,
+            userStats: {
+                dailyCalorieGoal: user.dailyCalorieGoal,
+                dailyConsumptionStats: user.dailyConsumptionStats
+            }
         });
 
     } catch (error) {
@@ -49,4 +74,5 @@ const getMenu = async (req, res) => {
         await client.close();
     }
 };
+
 module.exports = {getMenu}
