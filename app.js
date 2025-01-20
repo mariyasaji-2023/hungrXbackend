291412c290da2462a6c2a7f4f0050b8f80a60ce2
mariyasaji-2023 <mariyasajichiramel.com>
2025-01-20 10:26:38 +0530

    profile

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use('/users', userRoutes);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
